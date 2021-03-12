4a291856d5541e2b1417c417475248219d8ae02f
YannyKyprianou <yannykyps@hotmail.com>
2021-03-12 14:21:26 +0000

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Layout from "../components/Layout"
 import SEO from "../components/SEO"
 import Hero from "../components/Hero"
 import Feature from "../components/Feature"
-import Cta from "../components/cta"
+import Cta from "../components/Cta"
 
 const IndexPage = () => (
   <Layout>
